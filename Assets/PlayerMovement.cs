b6095c40a74932c3cc539d288621f3c887a9f5e7
Declan McKelvey-Hembree <djmckelv@wustl.edu>
2015-02-04 23:58:40 -0600

    Initial Commit with camera functionality and partial player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rotate90 target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int orientation = target.orientation;
+		if(Input.GetKeyDown("up")){
+			if(orientation==0){
+				transform.Translate(1,0,0, Space.World);
+			}else if(orientation==1){
+				transform.Translate(0,0,1, Space.World);
+			}else if(orientation==2){
+				transform.Translate(-1,0,0, Space.World);
+			}else{
+				transform.Translate(0,0,-1, Space.World);
+			}
+		}else if(Input.GetKeyDown("down")){
+			if(orientation==0){
+				transform.Translate(-1,0,0, Space.World);
+			}else if(orientation==1){
+				transform.Translate(0,0,-1, Space.World);
+			}else if(orientation==2){
+				transform.Translate(1,0,0, Space.World);
+			}else{
+				transform.Translate(0,0,1, Space.World);
+			}
+		}else if(Input.GetKeyDown("left")){
+			if(orientation==0){
+				transform.Translate(0,0,1, Space.World);
+			}else if(orientation==1){
+				transform.Translate(-1,0,0, Space.World);
+			}else if(orientation==2){
+				transform.Translate(0,0,-1, Space.World);
+			}else{
+				transform.Translate(1,0,0, Space.World);
+			}
+		}else if(Input.GetKeyDown("right")){
+			if(orientation==0){
+				transform.Translate(0,0,-1, Space.World);
+			}else if(orientation==1){
+				transform.Translate(1,0,0, Space.World);
+			}else if(orientation==2){
+				transform.Translate(0,0,1, Space.World);
+			}else{
+				transform.Translate(-1,0,0, Space.World);
+			}
+		}
+	}
+}
---
+++ Assets/Rotate90.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate90 : MonoBehaviour {
+	public int orientation;
+	public float desiredRotation;
+	public float turnDir; //1 to turn left, -1 to turn right, 0 to not turn
+	// Use this for initialization
+	void Start () {
+		orientation=0;
+		desiredRotation=transform.rotation.eulerAngles.y;
+		turnDir=0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yRotation = 90.0f;
+		if(Input.GetKeyDown("a")){
+			Debug.Log ("Left key pressed!");
+			desiredRotation=(float)mod((int)(desiredRotation+yRotation),360);
+			turnDir=1.0f;
+			orientation=mod(orientation-1,4);
+		}else if(Input.GetKeyDown ("d")){
+			Debug.Log ("Right key pressed!");
+			desiredRotation=(float)mod((int)(desiredRotation-yRotation),360);
+			turnDir=-1.0f;
+			orientation=mod(orientation+1,4);
+		}
+		float curAngle=transform.rotation.eulerAngles.y;
+		if(Mathf.Abs(curAngle-desiredRotation) > 3){
+			transform.rotation = Quaternion.AngleAxis(curAngle + turnDir*(Time.deltaTime * 45f), Vector3.up);
+			Debug.Log ("Current Angle: "+curAngle+" Desired Angle: "+desiredRotation);
+		}else if(Mathf.Abs(curAngle-desiredRotation) <= 3 && turnDir!=0.0f){
+			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,desiredRotation,transform.rotation.eulerAngles.z);
+			Debug.Log ("Snapped to Angle: " + desiredRotation);
+			turnDir=0.0f;
+		}
+	}
+	int mod(int x, int m) { //Custom modulo function. Do not just use %, negative numbers are a trap.
+    	return (x%m + m)%m;
+	}
+}
