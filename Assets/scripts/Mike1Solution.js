49db966261298f160eec1d7c21c292a2da42c4ca
Nicholas X <seanbean1992@gmail.com>
2015-02-19 14:16:24 -0600

    Revert e7681fd..ad33a65
    This rolls back to commit e7681fdcfdb8d4636247609993cdac43463ad129.

--- Assets/scripts/Mike1Solution.js
+++
@@ -1,8 +0,0 @@
-﻿/*
-
-Player starts at green, progresses by changing perspective to the purple button.
-Player should activate the button at the perspective that makes the ball appear
-to sit at the top of the "slope" made up of staggered cubes. The ball rolls onto
-the purple cube, pushing the red block up to replace the green one. The player
-then makes his way back to where the green block used to be to clear the level.
-*/
