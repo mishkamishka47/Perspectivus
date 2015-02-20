60dd7d48d13588fe29d39e8f144e04e1e45ef71a
Wenbin <seanbean1992@gmail.com>
2015-02-20 14:56:45 +0800

    start menu now doing well

--- Assets/scripts/startMenu.js
+++ Assets/scripts/startMenu.js
@@ -1,5 +1,71 @@
 ﻿#pragma strict
+import UnityEngine.UI;
 
+public var labelSkin : GUISkin;
+private var maxLevel : int = 8;
+private var firstMenu : boolean = true;
+private var current : int = 1;
+private var name : String;
+function OnGUI(){
+	GUI.skin = labelSkin;
+	if(firstMenu){
+		GUILayout.BeginArea(Rect(0, Screen.height*0.3, Screen.width, Screen.height*0.2));
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Perspective");
+		GUILayout.EndArea();
+		GUILayout.BeginArea(Rect(Screen.width*0.4, Screen.height*0.5, Screen.width*0.2, Screen.height*0.3));
+		GUILayout.BeginHorizontal();
+		GUILayout.BeginVertical();
+
+		if(GUILayout.Button("Play!!!")){
+			Application.LoadLevel("Tutorial1");
+		}
+		GUILayout.Space(20);
+		if(GUILayout.Button("load")){
+			firstMenu = false;
+		}
+		GUILayout.Space(20);
+		if(GUILayout.Button("quit")){
+			Application.Quit();
+		}
+		GUILayout.EndArea();
+	}
+	if(!firstMenu){
+		GUILayout.BeginArea(Rect(Screen.width*0.4, Screen.height*0.2, Screen.width*0.2, Screen.height*0.5));
+		GUILayout.BeginVertical();
+		if(current >= maxLevel){
+		
+		}
+		
+		var i = current;
+		
+		while(i<=(5+current) && i<=maxLevel){
+			if(GUILayout.Button("level"+i)){
+				Application.LoadLevel("Tutorial"+i);
+			}
+			GUILayout.Space(20);
+			i++;
+		}
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
+		GUILayout.BeginArea(Rect(0, Screen.height*0.8, Screen.width, Screen.height*0.2));
+		GUILayout.BeginHorizontal();
+		if(GUILayout.Button("Last")){
+			if(current > 6)
+				current -= 6;
+		}
+		if(GUILayout.Button("Back")){
+			current = 1;
+			firstMenu = true;
+		}
+		if(GUILayout.Button("Next")){
+			if(current < maxLevel)
+				current += 6;
+		}
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+	}
+}
 function Start () {
 
 }
