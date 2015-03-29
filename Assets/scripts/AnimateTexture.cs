93d5f9b50be98c863b34eef4b676da7276d6dec3
Mike Mishkin <mishkin.mike@gmail.com>
2015-03-29 17:09:14 -0500

    animating water texture
    conveniently applied to the first seven levels

---
+++ Assets/scripts/AnimateTexture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateTexture : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private Material shader;
+
+	// Use this for initialization
+	void Start () {
+		shader = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var currentOffset = shader.GetTextureOffset ("_MainTex");
+		currentOffset += new Vector2 (Time.deltaTime * speed, Time.deltaTime * speed);
+		shader.SetTextureOffset ("_MainTex", currentOffset);
+	}
+}
