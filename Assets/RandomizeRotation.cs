cda1b59598db55dd6853503ed96c933eba7af5da
Mike Mishkin <mishkin.mike@gmail.com>
2015-04-16 16:10:59 -0500

    Finished present levels
    All current levels have updated graphics

---
+++ Assets/RandomizeRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int x = Mathf.FloorToInt (Random.Range (0, 4)), y = Mathf.FloorToInt (Random.Range (0, 4)), z = Mathf.FloorToInt (Random.Range (0, 4));
+		transform.rotation = Quaternion.Euler (90 * x, 90 * y, 90 * z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
