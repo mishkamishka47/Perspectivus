4ed90e0ba681295812ba254f9c9552d8f06c255e
Mike Mishkin <mishkin.mike@gmail.com>
2015-04-16 12:34:51 -0500

    SICK NASTY ICE AND SNOW ASSETS

---
+++ Assets/RandomizeYRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeYRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var multiplier = Mathf.FloorToInt (Random.Range (0, 4));
+		transform.rotation = Quaternion.Euler (0, 90 * multiplier, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
